b8a0fe17e95e5ef14a18abb2d2d2461df2cadcee
Yahel Nachum <yahelnachum@yahoo.com>
2016-03-21 08:42:52 -0400

    microphone to file to service works still need to figure out streaming

--- src/testIBMSpeechToTextApp/TestMicrophoneToIBMServiceStreaming.java
+++ src/testIBMSpeechToTextApp/TestMicrophoneToIBMServiceStreaming.java
@@ -1,10 +1,35 @@
 package testIBMSpeechToTextApp;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+
 public class TestMicrophoneToIBMServiceStreaming {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+   private static CountDownLatch lock = new CountDownLatch(1);
+ 
+   public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+     SpeechToText service = new SpeechToText();
+     service.setUsernameAndPassword("<username>", "<password>");
+ 
+     FileInputStream audio = new FileInputStream("src/test/resources/speech_to_text/sample1.wav");
+ 
+     RecognizeOptions options = new RecognizeOptions();
+     options.continuous(true).interimResults(true).contentType(HttpMediaType.AUDIO_WAV);
+ 
+     service.recognizeUsingWebSockets(audio, options, new BaseRecognizeDelegate() {
+       @Override
+       public void onMessage(SpeechResults speechResults) {
+         System.out.println(speechResults);
+         if (speechResults.isFinal())
+           lock.countDown();
+       }
+     });
+ 
+     lock.await(20000, TimeUnit.MILLISECONDS);
+   }
+ }
