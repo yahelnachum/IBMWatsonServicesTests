ee8e37dd7d1da38e8ad289d6870dcb0712a37c07
Yahel Nachum <yahelnachum@yahoo.com>
2016-04-08 14:08:58 -0400

    redoing streaming with clean build

--- SpeechToText/Streaming/README.txt
+++
@@ -1,3 +0,0 @@
-This is a node server / javascript client that will test 
-streaming audio to IBM Watson's speech to text service 
-for a transcript of the audio.
--- SpeechToText/Streaming/server.js
+++ SpeechToText/Streaming/server.js
@@ -1,30 +1,16 @@
-// Author Jillian Hennessy
-// Author Yahel Nachum
-// Website http://jh-yn-final-cs4241.herokuapp.com/
-
 var express = require('express');
-var path = require('path');
 var app = express();
 var port = process.env.PORT || 3000;
-var bodyParser = require("body-parser");
-var pg = require('pg');
-var fs = require('fs');
-var bcrypt = require('bcryptjs');
-
-app.use(bodyParser.urlencoded({extended: false}));
+var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
-});
-
-app.get('/myAudio', function(req, res) {
-  console.log('getting audio file');
-  var audioData = fs.readFileSync(path.join(__dirname, '/public/short_audio.flac'));
-  res.send(new Buffer(audioData, 'binary'));
-  console.log('sent audio file');
+	console.log('still good');
+	res.send('hello world');
+	res.end();
 });
 
 app.listen(port, function() {
-  console.log('App is listening on port ' + port);
+	  console.log('App is listening on port ' + port);
 });
