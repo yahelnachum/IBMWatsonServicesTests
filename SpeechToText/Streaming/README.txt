5874b695ce4cae0fdc939045ad7a981b723ae35d
Yahel Nachum <yahelnachum@yahoo.com>
2016-04-08 14:55:20 -0400

    added readme to explain project, added service credentials to JSON file

---
+++ SpeechToText/Streaming/README.txt
@@ -0,0 +1,3 @@
+This eclipse project includes a node server and javascript client that 
+is trying to stream audio to the IBM speech to text service to recieve 
+a transcript.
--- SpeechToText/Streaming/server.js
+++ SpeechToText/Streaming/server.js
@@ -2,13 +2,20 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var path = require('path');
+var fs = require('fs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
-	console.log('still good');
-	res.send('hello world');
+	console.log('entering: app.get("/")');
+	console.log('exiting : app.get("/")');
+});
+
+app.get('/test', function(req, res) {
+	console.log('entering: app.get("/test")');
+	console.log(fs.readFileSync(path.join(__dirname, "serviceVariables.json")).toString());
 	res.end();
+	console.log('exiting : app.get("/test")');
 });
 
 app.listen(port, function() {
