8e4729cd997f06ec9d806a367c92e87218db0893
Yahel Nachum <ynachum@wpi.edu>
2016-04-08 20:05:34 -0400

    stuck trying to find version of service, will need to deploy a cloud foundry app to be able to do this

--- SpeechToText/Streaming/server.js
+++ SpeechToText/Streaming/server.js
@@ -3,20 +3,34 @@ var app = express();
 var port = process.env.PORT || 3000;
 var path = require('path');
 var fs = require('fs');
+var extend = require('util')._extend;
+var watson = require('watson-developer-cloud');
+
+var config = {
+		   version: '<service_version>',
+		   url: '<service_url>',
+		   username: '<username>',
+		   password: '<password>'
+};
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
-	console.log('entering: app.get("/")');
-	console.log('exiting : app.get("/")');
 });
 
 app.get('/test', function(req, res) {
 	console.log('entering: app.get("/test")');
-	console.log(fs.readFileSync(path.join(__dirname, "serviceVariables.json")).toString());
+	var credentials = extend(config, getSpeechToTextCredentials());
+	var authorization = watson.authorization(getSpeechToTextCredentials());
 	res.end();
 	console.log('exiting : app.get("/test")');
 });
+
+function getSpeechToTextCredentials(){
+	var serviceVariables = fs.readFileSync(path.join(__dirname, "serviceVariables.json")).toString();
+	serviceVariables = JSON.parse(serviceVariables);
+	return serviceVariables.speech_to_text.credentials;
+}
 
 app.listen(port, function() {
 	  console.log('App is listening on port ' + port);
